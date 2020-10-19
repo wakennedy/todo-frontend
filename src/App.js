71377dcd2e748ce3c5462b29be2752ef7b3caccb
wakennedy <alexkenned@gmail.com>
2020-10-19 11:49:27 -0700

    newItemForm scaffold

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import ItemContainer from "./components/ItemContainer.js";
+import NewItem from "./components/NewItem.js";
 
 const ITEMURL = "http://localhost:3000/items";
 
@@ -18,12 +19,24 @@ class App extends Component {
         });
       });
   }
+  handleItemSubmit = (event) => {
+    event.preventDefault();
+
+    //create new item
+    let newItem = {};
+    newItem.name = event.target.name.value;
+    newItem.description = event.target.description.value;
+    newItem.category = event.target.category.value;
+
+    console.log(newItem);
+  };
   render() {
     return (
       <Fragment>
         <div className="wrapper">
           <div>Test </div>
           <ItemContainer items={this.state.items} />
+          <NewItem onItemSubmit={this.handleItemSubmit} />
         </div>
       </Fragment>
     );
--- src/components/newItem.js
+++ src/components/newItem.js
@@ -1,11 +1,30 @@
 import React, { Component, Fragment } from "react";
 
 export default class NewItem extends Component {
-    render() {
-        return (
-            <Fragment>
-                
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <div className="newItemForm">
+          <div className="task-form">
+            <form onSubmit={(event) => this.props.onItemSubmit(event)}>
+              <h1>Create a New Task</h1>
+              <p>Name:</p>
+              <input type="text" name="name" />
+              <p>Description:</p>
+              <input type="text" name="description" />
+              <p>Category:</p>
+              <input type="text" name="category" />
+              <br></br>
+              <input
+                type="submit"
+                name="submit"
+                value="Create"
+                className="submit"
+              />
+            </form>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
