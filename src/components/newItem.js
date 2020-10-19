260ee5c5f41cd5a4dba31bdf30ca2d6fc35fbbf5
wakennedy <alexkenned@gmail.com>
2020-10-19 11:16:24 -0700

    Update newItem.js

--- src/components/newItem.js
+++ src/components/newItem.js
@@ -1,1 +1,11 @@
 import React, { Component, Fragment } from "react";
+
+export default class NewItem extends Component {
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
+}
